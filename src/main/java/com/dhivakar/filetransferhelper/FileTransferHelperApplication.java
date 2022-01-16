93850323ba8306d3767fc82602db7ee7969bdb75
Dhivakar R M <dhivainfo084@gmail.com>
2022-01-16 22:01:50 +0530

    Initial commit

---
+++ src/main/java/com/dhivakar/filetransferhelper/FileTransferHelperApplication.java
@@ -0,0 +1,13 @@
+package com.dhivakar.filetransferhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransferHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTransferHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhivakar/filetransferhelper/FileTransferHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhivakar.filetransferhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransferHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
