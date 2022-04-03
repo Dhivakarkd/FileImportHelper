40c81d13f98ee7060e95812f94ac8ba95897ec25
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-04-03 13:18:15 +0530

    added No Work Listener

--- src/main/java/com/dhivakar/filetransferhelper/config/BatchConfiguration.java
+++ src/main/java/com/dhivakar/filetransferhelper/config/BatchConfiguration.java
@@ -5,8 +5,10 @@ import com.dhivakar.filetransferhelper.database.handler.CustomDataReader;
 import com.dhivakar.filetransferhelper.database.model.BatchInfo;
 import com.dhivakar.filetransferhelper.database.model.ImportRecord;
 import com.dhivakar.filetransferhelper.listener.JobCompletionListener;
+import com.dhivakar.filetransferhelper.listener.NoWorkListener;
 import com.dhivakar.filetransferhelper.processor.ImportProcessor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -27,6 +29,8 @@ public class BatchConfiguration {
     private CustomDataReader customDataReader;
     private CompositeItemWriter<ImportRecord> compositeItemWriter;
     private ImportProcessor filesProcessor;
+    @Autowired
+    private NoWorkListener noWorkListener;
 
     @Autowired
     public BatchConfiguration(StepBuilderFactory stepBuilderFactory, CustomDataReader customDataReader, CompositeItemWriter<ImportRecord> compositeItemWriter, ImportProcessor filesProcessor) {
@@ -39,15 +43,23 @@ public class BatchConfiguration {
     @Bean
     public Step readAndCopyFiles() {
 
-        return this.stepBuilderFactory.get("Read and Copy Files").<BatchInfo, ImportRecord>chunk(5).reader(customDataReader)
-                .processor(filesProcessor).writer(compositeItemWriter).build();
+        return this.stepBuilderFactory.get("Read and Copy Files").<BatchInfo, ImportRecord>chunk(5)
+                .reader(customDataReader)
+                .processor(filesProcessor)
+                .writer(compositeItemWriter)
+                .listener(noWorkListener)
+                .build();
     }
 
     @Bean
     public Job analyzeAndCopyLatestFilesJob(JobBuilderFactory jobBuilderFactory, JobCompletionListener listener) {
 
-        return jobBuilderFactory.get("Analyze and Copy Latest Files").incrementer(new RunIdIncrementer())
-                .listener(listener).start(readAndCopyFiles()).build();
+        return jobBuilderFactory.get("Analyze and Copy Latest Files")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .start(readAndCopyFiles()).on(ExitStatus.FAILED.getExitCode()).fail()
+                .from(readAndCopyFiles()).on(ExitStatus.COMPLETED.getExitCode()).end().build()
+                .build();
     }
 
 }
